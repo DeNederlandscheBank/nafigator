783ac2d2c13ba4d1608daf72103417c8a4732480
Hyo Mee Duerinck <h.m.duerinck@dnb.nl>
2023-01-23 16:43:34 +0100

    indents, improvement test_convert_pdf, add test document

--- tests/test_preprocessprocessor.py
+++ tests/test_preprocessprocessor.py
@@ -11,35 +11,36 @@ unittest.TestLoader.sortTestMethodsUsing = None
 
 # Needed documents: pdf with and without password
 
-with open('tests/tests/convertor_output.json') as data_file:
-    exp_output_dict = json.load(data_file)
+with open('tests/tests/pdf_convertor_output.json') as data_file:
+    pdf_exp_output = json.load(data_file)
 
 
-@pytest.mark.parametrize("format, exp_output, params",
-                         [('text', exp_output_dict['pdf2text'], {'fileDesc': {'author': 'anonymous'},
-                                                                 'parse_tables_with_camelot': True}),
-                          ('xml', exp_output_dict['pdf2xml'], {'fileDesc': {'author': 'anonymous'}}),
-                          ('html', exp_output_dict['pdf2html'], {'fileDesc': {'author': 'anonymous'}})])
-def test_convert_pdf(format: str,  exp_output: str, params: dict):
+@pytest.mark.parametrize("format, params",
+                         [('text', {'fileDesc': {'author': 'anonymous'},
+                                    'parse_tables_with_camelot': True}),
+                          ('xml', {'fileDesc': {'author': 'anonymous'}}),
+                          ('html', {'fileDesc': {'author': 'anonymous'}})])
+def test_convert_pdf(format: str, params: dict):
     """
-        This function converts a pdf file into text, html or xml.
-        Input:
-            path: location of the file to be converted
-            format: html, text or xml
-            codec: codec to be used to conversion
-            password: password to be used for conversion
-            params: the general params dict to store results
-        Level: 0
-        Scenarios:
-            conversion to text
-            conversion to html
-            conversion to xml
+    This function converts a pdf file into text, html or xml.
+    Input:
+        path: location of the file to be converted
+        format: html, text or xml
+        codec: codec to be used to conversion
+        password: password to be used for conversion
+        params: the general params dict to store results
+    Level: 0
+    Scenarios:
+        conversion to text
+        conversion to html
+        conversion to xml
     """
     path = 'tests/tests/example.pdf'
     convert_pdf(path=path, format=format, params=params)
     if params.get('parse_tables_with_camelot', False):
         assert type(params["pdftotables"]) == camelot.core.TableList
-    assert params["pdfto" + format] == exp_output
+    output_key = "pdfto" + format
+    assert params[output_key] == pdf_exp_output[output_key]
 
 
 def test_convert_docx():
